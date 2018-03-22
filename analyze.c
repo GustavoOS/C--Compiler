4035a18b8da762e28637fc18b2f726eaeee93c72
Gustavo Oliveira de Souza <gustavo.oliveira.souza@outlook.com>
2018-03-21 22:15:55 -0300

    remove string to char * warnings

--- analyze.c
+++ analyze.c
@@ -232,7 +232,7 @@ static void insertNode(TreeNode *t)
 
     case ReturnK:
     {
-      l = st_find(scope, "global");
+      l = st_find(scope, (char *) "global");
       switch (l->dtype)
       {
       case Void:
@@ -319,7 +319,7 @@ static void insertNode(TreeNode *t)
 //
 void mainVerify()
 {
-  BucketList myList = st_find("main", scope);
+  BucketList myList = st_find((char *)"main", scope);
   if (myList == NULL)
   {
     Error = TRUE;
@@ -367,7 +367,7 @@ static void checkNode(TreeNode *t)
     case OpK:
       if ((t->child[0]->type != Integer) ||
           (t->child[1]->type != Integer))
-        typeError(t, "Op applied to non-integer");
+        typeError(t,(char *) "Op applied to non-integer");
       switch (t->attr.op)
       {
       case LESSEQ:
@@ -405,18 +405,18 @@ static void checkNode(TreeNode *t)
     case IfK:
       if (t->child[0] == NULL)
       {
-        typeError(t->child[0], "if test invalid");
+        typeError(t->child[0], (char *) "if test invalid");
       }
       else if (t->child[0]->type != boolean)
-        typeError(t->child[0], "if test is not boolean");
+        typeError(t->child[0], (char *) "if test is not boolean");
       break;
     case AssignK:
       if (t->child[1]->type != Integer)
-        typeError(t->child[1], "assignment of non-integer value");
+        typeError(t->child[1], (char *) "assignment of non-integer value");
       break;
     case WhileK:
       if (t->child[0]->type != boolean)
-        typeError(t->child[0], "while test is not boolean");
+        typeError(t->child[0], (char *) "while test is not boolean");
       break;
     default:
       break;
