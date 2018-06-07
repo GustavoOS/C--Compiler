91e8d588da7d7a9685fbd25d1932ec1bc4f8b75e
Gustavo Souza <gsouza@fitec.org.br>
2018-06-06 22:29:37 -0300

    empty code gen

--- codegenerator.h
+++ codegenerator.h
@@ -5,54 +5,5 @@
 #include <iostream>
 #include "globals.h"
 
-enum Operation
-{
-    sum,
-    multiply,
-    subtract,
-    divide,
-    accessVector,
-    functionCall /*TO DO*/
-};
-
-enum OperandType{
-    referenceOperand,
-    numberOperand,
-    variableOperand
-};
-
-class Operand {
-    public:
-    OperandType type;
-    int tripleIndex;
-    int number;
-    std::string name;
-    std::string scope;
-    void setAsVariable(std::string name, std::string scope);
-    void setAsNumber(int no);
-    void setAsReference(int index);
-};
-
-class Triple
-{
-
-  public:
-    Triple(Operation _operation,  Operand *_firstOPerand,
-            Operand *_secondOPerand);
-
-  private:
-    Operation operation;
-    Operand *firstOperand, *secondOperand;
-};
-
-class TripleList
-{
-    std::vector<Triple *> list;
-
-  public:
-    void addTriple(Triple *tripleItem);
-};
-void verifyNode(TreeNode * syntaxTree);
-void generateIntermediate(TreeNode *syntaxTree);
 
 #endif
