15e5989b60995c7a062f19e38e9e58ee93292a2c
Gustavo Souza <gsouza@fitec.org.br>
2018-06-11 02:42:31 -0300

    Instruction

--- codegenerator.cc
+++ codegenerator.cc
@@ -4,6 +4,7 @@
 #include "symtab.h"
 #include <iostream>
 
+//Code Generator Class
 CodeGenerator::CodeGenerator(bool displayable)
 {
     shouldPrintGeneratedCodeOnScreen = displayable;
@@ -86,3 +87,86 @@ void CodeGenerator::createFooter()
 {
     std::cout << "This is a footer\n";
 }
+
+//Instruction Class
+std::string Instruction::to_string()
+{
+    return "Generic Instruction";
+}
+
+class TypeAInstruction : public Instruction
+{
+    std::string to_string()
+    {
+        return "(" + std::to_string(id) + ") " +
+               name + " " + std::to_string(immediate) + " " +
+               std::to_string(regm) + " " + std::to_string(regd) + "\n";
+    }
+};
+
+class TypeBInstruction : public Instruction
+{
+    std::string to_string()
+    {
+        return "(" + std::to_string(id) + ") " +
+               name + " " + std::to_string(regm) + " " +
+               std::to_string(regn) + " " + std::to_string(regd) + "\n";
+    }
+};
+
+class TypeCInstruction : public Instruction
+{
+    std::string to_string()
+    {
+        return "(" + std::to_string(id) + ") " +
+               name + " " + std::to_string(immediate) + " " +
+               std::to_string(regm) + " " + std::to_string(regd) + "\n";
+    }
+};
+
+class TypeDInstruction : public Instruction
+{
+    std::string to_string()
+    {
+        return "(" + std::to_string(id) + ") " +
+               name + " " + std::to_string(regd) + " " +
+               std::to_string(immediate) + "\n";
+    }
+};
+
+class TypeEInstruction : public Instruction
+{
+    std::string to_string()
+    {
+        return "(" + std::to_string(id) + ") " +
+               name + " " + std::to_string(regm) + " " +
+               std::to_string(regd) + "\n";
+    }
+};
+
+class TypeFInstruction : public Instruction
+{
+    std::string to_string()
+    {
+        return "(" + std::to_string(id) + ") " +
+               name + " " + std::to_string(condition) + " " +
+               std::to_string(regd) + "\n";
+    }
+};
+
+class TypeGInstruction : public Instruction
+{
+    std::string to_string()
+    {
+        return "(" + std::to_string(id) + ") " +
+               name + " " + std::to_string(condition) + " " +
+               std::to_string(offset) + " :" + label + "\n";
+    }
+};
+
+class LabelDefinition: public Instruction
+{
+    std::string to_string(){
+        return label + ":\n";
+    }
+};
--- codegenerator.h
+++ codegenerator.h
@@ -4,34 +4,44 @@
 #include <vector>
 #include <iostream>
 #include "globals.h"
+#include <vector>
 
 enum Registers
 {
-    HeapArrayRegister = 1,
-    AcumulatorRegister,
-    TemporaryRegister,
-    FramePointer,
-    GlobalPointer
+  HeapArrayRegister = 1,
+  AcumulatorRegister,
+  TemporaryRegister,
+  FramePointer,
+  GlobalPointer
+};
+
+class Instruction
+{
+public:
+  int id;
+  std::string name, label;
+  int regm, regn, regd, condition, immediate, offset;
+  std::string to_string();
 };
 
 class CodeGenerator
 {
-  public:
-    CodeGenerator(bool displayable);
-    void generate(TreeNode *node);
+public:
+  CodeGenerator(bool displayable);
+  void generate(TreeNode *node);
 
-  private:
-    std::string generatedCode;
-    bool shouldPrintGeneratedCodeOnScreen;
+private:
+  std::string generatedCode;
+  std::vector<Instruction> code;
+  bool shouldPrintGeneratedCodeOnScreen;
 
-    //Private methods
-    void print(std::string code);
-    void generateCode(TreeNode *node);
-    void createHeader();
-    void createFooter();
-    void generateCodeForStmtNode(TreeNode *node);
-    void generateCodeForExprNode(TreeNode *node);
-
+  //Private methods
+  void print(std::string code);
+  void generateCode(TreeNode *node);
+  void createHeader();
+  void createFooter();
+  void generateCodeForStmtNode(TreeNode *node);
+  void generateCodeForExprNode(TreeNode *node);
 };
 
 #endif
