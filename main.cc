463af78980b90eb4d9564dfc5aa89394cbe7edd0
Gustavo Souza <gustavo.oliveira.souza@outlook.com>
2019-12-29 13:55:12 -0300

    Return Main flow to compressed program

--- main.cc
+++ main.cc
@@ -48,9 +48,9 @@ int TraceCode = TRUE;
 int Error = FALSE;
 
 // Mark True for OS mode development
-bool isOperatingSystem = true;
+bool isOperatingSystem = false;
 bool isBios = false;
-bool isCompressedProgram = false;
+bool isCompressedProgram = true;
 
 int PROGRAM_OFFSET = 0;
 
